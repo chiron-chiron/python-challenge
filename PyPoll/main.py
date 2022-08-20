8229e693e7df2b95e34903d4960ab78d9eeaab41
chiron-chiron <84137247+chiron-chiron@users.noreply.github.com>
2022-08-20 15:26:53 +1000

    WIP

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,27 @@
 import os
 import csv
 
+# Set file for path
 # election_data = 'Resources/book2.csv'
 csvpath = os.path.join("Resources", "election_data.csv")
 
-rowcount = 0
-Voter_ID = []
-County = []
-Candidate_list = []
+
+
+# Set variable
+rowcount = 0                # Integer counting number of votes
+voter_ID = []               
+county = []
+candidate_list = []
+
+# Dictionary of each candidates votes
+counter = {
+    "Correy": 0,
+    "Khan": 0,
+    "Li": 0,
+    "OTooley": 0
+}
+
+
 
 # Open the csv file in read mode
 with open(csvpath) as csvfile:
